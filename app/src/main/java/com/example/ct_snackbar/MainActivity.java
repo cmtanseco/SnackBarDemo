6ffe03b9869a467ba86b950b8d79585c5b985cb6
Carlos Tanseco <cmtanseco@gmail.com>
2019-03-08 15:31:55 -0600

    Added example for customizing a snackbar

--- app/src/main/java/com/example/ct_snackbar/MainActivity.java
+++ app/src/main/java/com/example/ct_snackbar/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.ct_snackbar;
 
+import android.graphics.Color;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnFirst, btnSecond;
+    private Button btnFirst, btnSecond, btnThird;
     private CoordinatorLayout coordinatorlayout;
 
     @Override
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnFirst = findViewById(R.id.btnFirst);
         btnSecond = findViewById(R.id.btnSecond);
+        btnThird = findViewById(R.id.btnThird);
         coordinatorlayout = findViewById(R.id.coordinatorlayout);
 
         btnFirst.setOnClickListener(new View.OnClickListener() {
@@ -37,12 +41,29 @@ public class MainActivity extends AppCompatActivity {
                                             .setAction("Undo", new View.OnClickListener() {
                                                 @Override
                                                 public void onClick(View v) {
-
                                                 }
                                             });
                 snackbar.show();
             }
         });
 
+        btnThird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar snackbar = Snackbar
+                        .make(coordinatorlayout, "Image Downloaded", Snackbar.LENGTH_LONG)
+                        .setAction("Open", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                            }
+                        });
+
+                snackbar.setActionTextColor(Color.BLUE);
+                View sbView = snackbar.getView();
+                TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setTextColor(Color.GREEN);
+                snackbar.show();
+            }
+        });
     }
 }
